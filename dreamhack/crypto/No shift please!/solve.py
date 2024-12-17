e5ae4ae61c750f36a684dbb7c844e606db7f785c
Winter <133123465+sup3rshy@users.noreply.github.com>
2024-12-17 20:11:09 +0700

    Update solve.py

--- dreamhack/crypto/No shift please!/solve.py
+++ dreamhack/crypto/No shift please!/solve.py
@@ -4,17 +4,17 @@ from pwn import *
 tmp = b'\x00' * 16
 
 # p = process(['python3', 'chall.py'])
-p = remote('host3.dreamhack.games', 24535)
+p = remote('host3.dreamhack.games', 9085)
 p.recvuntil(b'enc(secret) = ')
 enc_secret = bytearray(bytes.fromhex(p.recvline()[:-1].decode()))
 arr = []
-for i in range(4):
+for i in range(2):
     tmp = bytearray(b'\x00' * 16)
-    tmp[i * 4 : i * 4 + 4] = enc_secret[i * 4 : i * 4 + 4]
+    tmp[i * 8 : i * 8 + 8] = enc_secret[i * 8 : i * 8 + 8]
     p.sendlineafter(b'decrypt: ', b'2')
     p.sendlineafter(b'hex: ', tmp.hex())
     p.recvuntil(b'dec(ciphertext) = ')
-    arr.append(bytes.fromhex(p.recvline()[:-1].decode())[i * 4 : i * 4 +4])
+    arr.append(bytes.fromhex(p.recvline()[:-1].decode())[i * 8 : i * 8 + 8])
 
 secret = b''
 for x in arr:
