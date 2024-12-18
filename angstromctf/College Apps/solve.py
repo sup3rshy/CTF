739ecc9d66d3e442ba49cf6465c3a913b6e3f00e
Winter <133123465+sup3rshy@users.noreply.github.com>
2024-12-18 19:31:55 +0700

    Update solve.py

--- angstromctf/College Apps/solve.py
+++ angstromctf/College Apps/solve.py
@@ -25,3 +25,4 @@ while arr[-1] != ord('}'):
         if ((x & 0xff000000) == 0xbc000000) and ((x & 0xff) == len(arr)):
             arr.append((x >> 16) & 0xff)
     print(bytes(arr))
+# actf{now_fork_over_your_100k_annual_tuition}
