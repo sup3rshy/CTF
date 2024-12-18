3053688462b241c110abd1d83515a7a94291f6b3
Winter <133123465+sup3rshy@users.noreply.github.com>
2024-12-18 19:31:33 +0700

    Create solve.py

---
+++ angstromctf/College Apps/solve.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from pwn import * 
+from ctypes import * 
+
+txt = open("admissions-database.txt", "r").read().split(' ')[:-1]
+arr = []
+idx = 0
+for i in range(len(txt)):
+    x = txt[i].split(':')
+    if len(x) < 2:
+        continue
+    x = int(x[1], 8)
+    x ^= 0x4A72B18C
+    if ((x & 0xff000000) == 0xbc000000) and ((x & 0xff) == len(arr)):
+        arr.append((x >> 16) & 0xff)
+
+print(bytes(arr))
+while arr[-1] != ord('}'):
+    for i in range(len(txt)):
+        x = txt[i].split(':')
+        if len(x) < 2:
+            continue
+        x = int(x[1], 8)
+        x ^= 0x4A72B18C
+        if ((x & 0xff000000) == 0xbc000000) and ((x & 0xff) == len(arr)):
+            arr.append((x >> 16) & 0xff)
+    print(bytes(arr))
